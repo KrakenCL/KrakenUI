f137ccdca8f4fc982b647069e2a4b4e7b1a03dcb
Volodymyr Pavliukevych <volodymyr@octadero.com>
2019-01-24 18:12:22 +0200

    Add Job and Trigger UI.

--- ui-src/pages/Job.vue
+++ ui-src/pages/Job.vue
@@ -1,18 +1,137 @@
 <template>
     <section class="page page--ui-job">
-        <h2 class="page__title">job</h2>
+        <h2 class="page__title"> Job {{ identifier }} </h2>
+        <h4> Job result:</h4>
+        <br>
+        <table class="table details-table">
+            <tbody>
+                <tr v-for="detail in job.details" :key="detail.title">
+                    <td class="details-table-title-collumn">
+                        {{ detail.title }}:
+                    </td>
+                    <td class="details-table-value-collumn">
+                        {{ detail.value }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <br>
+        <ui-collapsible title="General" close>
+                <h4> Language:</h4>
+                <code class="job-log-output">{{ job.general.language }}</code>
 
-        <p>That is future job {{ identifier }} </p>
+                <h4> Launch arguments:</h4>
+                <code class="job-log-output">{{ job.general.arguments }}</code>
+                <h4> PIP installed dependencies:</h4>
+                <code class="job-log-output">{{ job.general.pipInstalledDependencies }}</code>
+
+        </ui-collapsible>
+        <ui-collapsible title="Environment" close>
+                <h4> Environment settings:</h4>
+                <code class="job-log-output">
+                    <vue-custom-scrollbar class="scroll-area">{{ job.environment }}</vue-custom-scrollbar>
+                </code>
+        </ui-collapsible>
+        
+        <ui-collapsible title="Launch log output" clse>
+                <h4> Standard log output:</h4>
+                <code class="job-log-output">
+                    <vue-custom-scrollbar class="scroll-area">{{ job.stdOutput }}</vue-custom-scrollbar>
+                </code>
+                <br>
+                <ui-icon-button icon="get_app" color="primary"></ui-icon-button>
+
+                <h4> Error log output:</h4>
+                <code class="job-lo-error-output">
+                    <vue-custom-scrollbar class="scroll-area">{{ job.errorOutput }}</vue-custom-scrollbar>
+                </code>
+                <br>
+                <ui-icon-button icon="get_app" color="primary"></ui-icon-button>
+
+        </ui-collapsible>
     </section>
 </template>
 
 <script>
-console.log("Job load: " + new Date());
+import vueCustomScrollbar from 'vue-custom-scrollbar'
+import UiCollapsible from 'src/UiCollapsible.vue';
+import UiFileupload from 'src/UiFileupload.vue';
+import UiPreloader from 'src/UiPreloader.vue';
+import UiTextbox from 'src/UiTextbox.vue';
+import UiSelect from 'src/UiSelect.vue';
+import UiSwitch from 'src/UiSwitch.vue';
+import UiButton from 'src/UiButton.vue';
+import UiIconButton from 'src/UiIconButton.vue';
+import UiAlert from 'src/UiAlert.vue';
+import axios from 'axios'
+
 export default {
+    components: {
+        vueCustomScrollbar,
+        UiCollapsible,
+        UiIconButton,
+        UiFileupload,
+        UiPreloader,
+        UiTextbox,
+        UiSwitch,
+        UiButton,
+        UiSelect,
+        UiAlert
+    },
+    computed: {
+            // jobDetails: function () {
+            //     var details = {};
+            //     details['id'] = '1';
+            //     details['title'] = 'TITLEw';
+            //     return [details];
+            // }
+    },
     data() {
         return {
             identifier : this.$route.params.identifier,
+            job: {
+                details: [
+                    {
+                        title: 'Model',
+                        value: 'ResNet 2.4'
+                    },
+                    {
+                        title: 'Configuration',
+                        value: 'SimpleRun'
+                    },
+                    {
+                        title: 'Fired trigger',
+                        value: 'Scheduled 3:00PM'
+                    },
+                    {
+                        title: 'Launch time',
+                        value: '21 of Jan 2019 3:00:01PM UTC+0000'
+                    },
+                    {
+                        title: 'Result',
+                        value: 'Finished'
+                    },
+                    {
+                        title: 'DataSource',
+                        value: 'Home Google Drive'
+                    }
+                ],
+                status: {
+                    code: 100,
+                    message: 'Finished',
+                },
+                general: {
+                    language: 'python2',
+                    pipInstalledDependencies: ["numpy", "pandas"],
+                    arguments: ["--train", "--epoch", "1000", "--gpu-enable"],
+                },
+                environment: 'TERM_PROGRAM=Apple_Terminal\nTERM=xterm-256color\nSHELL=/bin/bash\nTMPDIR=/var/folders/pz/s7d9jjkj29ggcbwjdpbt48xr0000gn/T/\nApple_PubSub_Socket_Render=/private/tmp/com.apple.launchd.sxwkqvTyDL/Render\nTERM_PROGRAM_VERSION=421.1\nTERM_SESSION_ID=99F0BB5C-4DF9-481E-BDEE-F02EB7D0A29A\nUSER=roaming\nSSH_AUTH_SOCK=/private/tmp/com.apple.launchd.n1cgoxoBVq/Listeners\nPATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin\nPWD=/Users/roaming/Projects/KrakenUI\nXPC_FLAGS=0x0\nXPC_SERVICE_NAME=0\nHOME=/Users/roaming\nSHLVL=1\nLOGNAME=roaming\nLC_CTYPE=UTF-8\n_=/usr/bin/env',
+                stdOutput: 'default	15:06:55.779673 +0200	com.apple.WebKit.Networking	TIC TCP Conn Connected [1659:0x7f9c9a1c6420]: Err(16)\ndefault	15:06:55.779891 +0200	com.apple.WebKit.Networking	TIC TCP Conn Event [1659:0x7f9c9a1c6420]: 1\ndefault	15:06:55.780174 +0200	com.apple.WebKit.Networking	Task <DB6B6591-D200-456C-9547-49807102D53C>.<8810> now using Connection 1659\ndefault	15:06:55.780457 +0200	com.apple.WebKit.Networking	Task <DB6B6591-D200-456C-9547-49807102D53C>.<8810> sent request, body N\ndefault	15:06:55.835082 +0200	com.apple.WebKit.Networking	Task <DB6B6591-D200-456C-9547-49807102D53C>.<8810> received response, status 200 content C\ndefault	15:06:55.835591 +0200	com.apple.WebKit.Networking	0x112feab00 - NetworkResourceLoader::didReceiveResponse: (pageID = 124, frameID = 1, resourceID = 233, httpStatusCode = 200, length = -1)\ndefault	15:06:55.835833 +0200	com.apple.WebKit.Networking	0x112feab00 - NetworkResourceLoader::didReceiveBuffer: Started receiving data (pageID = 124, frameID = 1, resourceID = 233)\ndefault	15:06:55.836205 +0200	com.apple.WebKit.WebContent	0x520c75930 - WebResourceLoader::didReceiveResponse: (pageID = 124, frameID = 1, resourceID = 233, status = 200)\ndefault	15:06:55.836309 +0200	com.apple.WebKit.WebContent	0x520c75930 - WebResourceLoader::didReceiveData: Started receiving data (pageID = 124, frameID = 1, resourceID = 233)\ndefault	15:06:55.888471 +0200	com.apple.WebKit.Networking	Task <DB6B6591-D200-456C-9547-49807102D53C>.<8810> response ended\ndefault	15:06:55.888569 +0200	com.apple.WebKit.Networking	Task <DB6B6591-D200-456C-9547-49807102D53C>.<8810> done using Connection 1659\ndefault	15:06:55.889001 +0200	com.apple.WebKit.Networking	0x112feab00 - NetworkResourceLoader::didFinishLoading: (pageID = 124, frameID = 1, resourceID = 233, length = 12)\n',
+                errorOutput: 'default	15:06:55.779673 +0200	com.apple.WebKit.Networking	TIC TCP Conn Connected [1659:0x7f9c9a1c6420]: Err(16)\ndefault	15:06:55.779891 +0200	com.apple.WebKit.Networking	TIC TCP Conn Event [1659:0x7f9c9a1c6420]: 1\ndefault	15:06:55.780174 +0200	com.apple.WebKit.Networking	Task <DB6B6591-D200-456C-9547-49807102D53C>.<8810> now using Connection 1659\ndefault	15:06:55.780457 +0200	com.apple.WebKit.Networking	Task <DB6B6591-D200-456C-9547-49807102D53C>.<8810> sent request, body N\ndefault	15:06:55.835082 +0200	com.apple.WebKit.Networking	Task <DB6B6591-D200-456C-9547-49807102D53C>.<8810> received response, status 200 content C\ndefault	15:06:55.835591 +0200	com.apple.WebKit.Networking	0x112feab00 - NetworkResourceLoader::didReceiveResponse: (pageID = 124, frameID = 1, resourceID = 233, httpStatusCode = 200, length = -1)\ndefault	15:06:55.835833 +0200	com.apple.WebKit.Networking	0x112feab00 - NetworkResourceLoader::didReceiveBuffer: Started receiving data (pageID = 124, frameID = 1, resourceID = 233)\ndefault	15:06:55.836205 +0200	com.apple.WebKit.WebContent	0x520c75930 - WebResourceLoader::didReceiveResponse: (pageID = 124, frameID = 1, resourceID = 233, status = 200)\ndefault	15:06:55.836309 +0200	com.apple.WebKit.WebContent	0x520c75930 - WebResourceLoader::didReceiveData: Started receiving data (pageID = 124, frameID = 1, resourceID = 233)\ndefault	15:06:55.888471 +0200	com.apple.WebKit.Networking	Task <DB6B6591-D200-456C-9547-49807102D53C>.<8810> response ended\ndefault	15:06:55.888569 +0200	com.apple.WebKit.Networking	Task <DB6B6591-D200-456C-9547-49807102D53C>.<8810> done using Connection 1659\ndefault	15:06:55.889001 +0200	com.apple.WebKit.Networking	0x112feab00 - NetworkResourceLoader::didFinishLoading: (pageID = 124, frameID = 1, resourceID = 233, length = 12)\n'
+            }
         };
+    },
+    methods: {
     }
 };
 </script>
@@ -21,5 +140,37 @@ export default {
 @import '~styles/imports';
 
 .page--ui-job {
+    .job-log-output {
+        padding: 10px;
+        display: block;
+        white-space: pre;
+        background-color: lightgrey;
+        color: black;
+        
+    }
+    .job-lo-error-output {
+        padding: 10px;
+        display: block;
+        white-space: pre;
+        background-color: rgb(252, 218, 218);
+    }
+    .scroll-area {
+        position: relative;
+        margin: auto;
+    }
+    .details-table {
+        margin-bottom: rem-calc(12px);
+        min-width: rem-calc(200px);
+        max-width: rem-calc(600px);
+    }
+    .details-table-title-collumn {
+        width: 30%;
+        text-align: right;
+
+    }
+    .details-table-value-collumn {
+        width: 70%;
+    }
+
 }
 </style>
--- ui-src/pages/Trigger.vue
+++ ui-src/pages/Trigger.vue
@@ -1,15 +1,81 @@
 <template>
     <section class="page page--ui-trigger">
         <h2 class="page__title">Trigger</h2>
-        <h3> General triger settings: </h3>
-            <ui-switch v-model="triger.isEnable">That triger enabled</ui-switch>
+        <h3> General trigger settings: </h3>
+            <ui-switch v-model="trigger.isEnable"> Enabled </ui-switch>
 
             <ui-textbox
-                help="Triger short name"
+                help="Trigger short name"
                 label="Name"
                 placeholder="Scheduled 3:00PM"
-                v-model="triger.name"
+                v-model="trigger.name"
             ></ui-textbox>
+            <ui-textbox
+                help="Optional trigger description"
+                label="Description"
+                placeholder="Every day at night"
+                v-model="trigger.description"
+            ></ui-textbox>
+
+            <ui-switch v-model="trigger.schedule.isEnable"> Periodically schedule </ui-switch>
+            <br>
+            <div>
+                <table class="table daily-table">
+                    <tbody>
+                        <tr>
+                            <td>
+                                 <h4>Daily at:</h4>
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <ui-select
+                                    label="Hour"
+                                    placeholder="Select a hour"
+                                    :options="dailyHouts"
+                                    v-model="trigger.schedule.periodically[0].daily.hour"
+                                    :disabled="!trigger.schedule.isEnable"
+                                ></ui-select>
+                            </td>
+                            <td>
+                                <ui-select
+                                    label="Minute"
+                                    placeholder="Select a minut"
+                                    :options="dailyMinutes"
+                                    v-model="trigger.schedule.periodically[0].daily.minut"
+                                    :disabled="!trigger.schedule.isEnable"
+                                ></ui-select>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+            <ui-switch v-model="trigger.apicall.isEnable"> Launch by API call </ui-switch>
+            <ui-textbox
+                disabled
+                label="Path to API call to launch trigger"
+                v-model="trigger.apicall.path"
+            ></ui-textbox>
+            <br>
+
+            <ui-switch v-model="trigger.datasource.isEnable"> Launch by DataSource event </ui-switch>
+            <ui-select
+                label="DataSource"
+                placeholder="Select one of datasources"
+                :options="datasources"
+                v-model="trigger.datasource.identifier"
+                :disabled="!trigger.datasource.isEnable"
+            ></ui-select>
+            <ui-select
+                label="DataSource event"
+                placeholder="Select one of events"
+                :options="datasourceEvents"
+                v-model="trigger.datasource.event"
+                :disabled="!trigger.datasource.isEnable"
+            ></ui-select>            
+            <br>
     </section>
 </template>
 
@@ -30,9 +96,37 @@ export default {
     },
     data() {
         return {
+
             identifier : this.$route.params.identifier,
-            triger: {
+            dailyHouts: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"],
+            dailyMinutes: ["1", "5", "10", "15", "20", "25", "30", "35", "40", "45", "59"],
+            datasources: ["ArchivedFolder", "Home-GDisk", "Work-GDisk", "Google Cloud bucket"],
+            datasourceEvents: ["NewFileAvailable", "FileSizeChanged"],
+            trigger: {
                 isEnable: true,
+                name: '',
+                description: '',
+                apicall: {
+                    isEnable: true,
+                    path: '/api/trigger/launch/EB3F50A5-65FA-4579-813D-C46CEE30A9EB'
+                },
+                datasource: {
+                    isEnable: true,
+                    identifier: null,
+                    event: null,
+                },
+                schedule: {
+                    isEnable: true,
+                    periodically: [
+                        {
+                            daily: {
+                                hour: "15",
+                                minut: "30",
+                            }
+                        }
+                    ],
+                }
+
             }
         };
     },
@@ -57,5 +151,13 @@ export default {
 @import '~styles/imports';
 
 .page--ui-trigger {
+    .daily-table {
+        margin-bottom: rem-calc(12px);
+        max-width: rem-calc(200px);
+    }
+    .ui-select {
+        margin-bottom: rem-calc(32px);
+        max-width: rem-calc(400px);
+    }
 }
 </style>
